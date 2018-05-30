0f128153ed479c2a1cfade21d5b45fd592b9475e
nimetapaydin <nimetapaydin>
2018-05-30 17:39:16 +0300

    firebase işlemleri eklendi

---
+++ app/src/main/java/com/example/nimet/bestday/data/BestdayQueryHandler.java
@@ -0,0 +1,15 @@
+package com.example.nimet.bestday.data;
+
+
+import android.content.AsyncQueryHandler;
+import android.content.ContentResolver;
+
+
+public class BestdayQueryHandler extends AsyncQueryHandler {
+
+
+        public BestdayQueryHandler(ContentResolver cr) { super(cr);}
+
+
+}
+
